c09957a0ce401d1910d5b92dc1fe36b7945de5df
leighad <cdversiontwo@gmail.com>
2020-06-21 18:58:49 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function() {
+    loadImages();
+});
+
+function loadImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+        .then(res => res.json())
+        .then(data => {
+            data.message.forEach(image => addImage(image))
+        });
+}
+
+function addImage(dogPicUrl) {
+    let container = document.querySelector("#dog-image-container");
+    let newImageEl = document.createElement("img");
+    newImageEl.src = dogPicUrl;
+    container.appendChild(newImageEl);
+}
+
